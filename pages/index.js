163eba8ace68725977f971255751a35654346220
ali mert inal <mert.inal@gmail.com>
2023-04-18 00:16:26 +0200

    add second part

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,21 @@ export default function Home() {
     <main className='bg-white px-10'>
       <section className='min-h-screen'>
         <nav className='py-10 mb-12 flex justify-between'>
-        <h1 className='text-xl'>Hello</h1>
+        <h1 className='text-xl font-burtons'>developedByMert</h1>
         <ul className='flex items-center'>
           <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
           <li> <a href="#" className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'>Resume</a> </li>
           
         </ul>
         </nav>
+        <div>
+          <h2>Ali Mert Inal</h2>
+          <h3>Frontend Developer and UX Designer/Researcher</h3>
+          <p>Freelancer providing you solutions for your webdev problems, wishes, requests... Ask me from research to code, let me help you realize it!</p>
+        </div>
+        <div>
+          
+        </div>
       </section>
     </main>
     </>
